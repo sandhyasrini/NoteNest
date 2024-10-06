b2dab265e3739c70287a0b5ea64cdc3e2b302f95
Sandhya Srinivasan <sandhya.srinivasan@advantest.com>
2024-10-06 12:05:31 +0200

    nextui installation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { NextUIProvider } from "@nextui-org/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -12,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <NextUIProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </NextUIProvider>
+
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,16 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
-  content: ["./app/**/*.{ts,tsx,mdx}"],
-  theme: {},
-  plugins: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],  theme: {},
+  plugins: [nextui()],
 };
 export default config;
