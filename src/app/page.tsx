4e6811db618dd13372fa8e282d8de5a621794643
Sandhya Srinivasan <sandhya.srinivasan@advantest.com>
2024-10-06 11:54:46 +0200

    added dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </main>
   );
 }
